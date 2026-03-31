1d8f4ab2c449bf1286700c2d2970070f02ac4c38
bro <bro@beam-mw.com>
2026-03-31 15:06:17 +0200

    feat: plausible

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { init as initPlausible } from '@plausible-analytics/tracker';
 
 import { App } from './App';
 import './styles.css';
+
+initPlausible({
+  domain: 'https://wykra.io/',
+  autoCapturePageviews: true,
+  outboundLinks: true,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
